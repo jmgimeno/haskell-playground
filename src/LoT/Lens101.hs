6adf6bf2638adb865b5f2fde00c36332d14cfbde
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2017-08-20 12:02:11 +0200

    Some alternative solutions using Control.Arrow

--- src/LoT/Lens101.hs
+++ src/LoT/Lens101.hs
@@ -2,6 +2,8 @@
 
 module LoT.Lens101 where
 
+import Control.Arrow
+
 type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
 type Lens' s a = Lens s s a a
 
@@ -29,11 +31,13 @@ choosing _  l2 f (Right s2) = Right <$> l2 f s2
 -- do it without lambdas and defining new functions. There's also a hint
 -- before the end of the section, so don't scroll if you don't want it.)
 (<%-) :: Lens s t a b -> (a -> b) -> s -> (b, t)
-(<%-) l f s = l (\a -> let b = f a in (b, b)) s
+--(<%-) l f s = l (\a -> let b = f a in (b, b)) s
+(<%-) l f = l ((id &&& id) . f)
 
 -- Modify the target of a lens, but return the old value.
 (<<%-) :: Lens s t a b -> (a -> b) -> s -> (a, t)
-(<<%-) l f s = l (\a -> (a, f a)) s
+--(<<%-) l f s = l (\a -> (a, f a)) s
+(<<%-) l f = l (id &&& f)
 
 -- There's a () in every value. (No idea what this one is for, maybe it'll
 -- become clear later.)
